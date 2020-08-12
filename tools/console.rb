51fb58fb32896534a03feadd468570e2357c3bc9
Caryn McCarthy <cmccarthy15@gmail.com>
2020-08-12 16:16:09 -0400

    first commit

---
+++ tools/console.rb
@@ -0,0 +1,13 @@
+# require and load the environment file
+require_relative '../config/environment.rb'
+
+# call this method to reload your models as you write code
+def reload
+  load 'config/environment.rb'
+end
+
+
+
+# ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
+binding.pry
+0
